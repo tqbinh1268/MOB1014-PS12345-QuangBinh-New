2a9ee03d29be2c42b546fb598abc4c4a5a5aaacb
tqbinh1268 <tqbinh@gmail.com>
2026-09-17 12:24:51 +0700

    Hoan thanh Lab 1 bai 1

---
+++ Lab1/src/main/java/com/poly/lab1/HelloJava.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.poly.lab1;
+
+/**
+ *
+ * @author ongmuoigiao
+ */
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Ho ten: Tran Quang Binh\nMSSV: PS12345");
+    }
+}
